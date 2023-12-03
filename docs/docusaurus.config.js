1e3163e3b67a46be618c9eae929cc1c4ad5579e4
nekochanfood <daliltexgamer@gmail.com>
2023-12-03 21:31:09 +0900

    commit

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'OSC Clock',
-  tagline: 'OSC経由で日付と時刻をアバターに送信',
+  tagline: 'OSC経由で日付と時刻を VRChat に送信',
   favicon: './static/favicon.ico',
 
   // Set the production url of your site here
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   return (
     <Layout
       title={`ホーム`}
-      description="Description will go into a meta tag in <head />">
+      description="OSC Clock のドキュメントサイト<head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
